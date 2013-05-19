8ad7706b1cbe7e2bd1a1fd6cbe9085a3f3e0586c
Nchinda Fam <nchindafam@Saturn.local>
2013-05-19 05:34:18 -0500

    Initial Commit

---
+++ Service Registry/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Service Registry
+//
+//  Created by Nchinda Fam on 5/19/13.
+//  Copyright (c) 2013 Nchinda Nchinda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Service RegistryTests/Service_RegistryTests.m
@@ -0,0 +1,32 @@
+//
+//  Service_RegistryTests.m
+//  Service RegistryTests
+//
+//  Created by Nchinda Fam on 5/19/13.
+//  Copyright (c) 2013 Nchinda Nchinda. All rights reserved.
+//
+
+#import "Service_RegistryTests.h"
+
+@implementation Service_RegistryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Service RegistryTests");
+}
+
+@end
