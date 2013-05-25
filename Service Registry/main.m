b7e549cc83d939f4acedbf2fbae3795798c85ed4
Nchinda Fam <nchindafam@Saturn.local>
2013-05-24 21:59:23 -0500

    This is the first save of the project-the GTMOAuth2 undefined error is unsolved and GDrive hasn't been written fully

--- Service Registry/main.m
+++ Service Registry/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "AppDelegate.h"
+#import "Service_RegistryAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Service_RegistryAppDelegate class]));
     }
 }
